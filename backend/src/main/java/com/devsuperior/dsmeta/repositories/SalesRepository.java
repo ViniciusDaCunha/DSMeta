ad179798e44e3872187a5da1a5fc9d33599bd0e9
Vinicius da Cunha <dacunhavini@gmail.com>
2022-07-13 13:24:45 -0300

    Data select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
@@ -8,4 +13,8 @@ public interface SalesRepository extends JpaRepository<Sale, Long>{
 /**
  reponsável por acessar o banco de dados
   **/
+
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+
 }
